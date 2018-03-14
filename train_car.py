6df5d228fe4cce08b9ff33fd945c8627d0c1ed14
sai007 <sai_praneeth7777@yahoo.co.in>
2018-03-14 19:55:47 +0530

    2nd

--- train_car.py
+++ train_car.py
@@ -3,7 +3,7 @@
 
 # In[1]:
 
-
+# Importing Dependencies
 import numpy as np
 import tensorflow as tf
 import pandas as pd
@@ -17,9 +17,9 @@ from keras.layers import Flatten,Dense,Lambda,Convolution2D,MaxPooling2D,Dropout
 
 # In[2]:
 
-
+# Input Data
 train = pd.read_csv('train/driving_log.csv',header=None)
-images = []
+images = [] # Initializing empty variables
 measurements = []
 
 
@@ -35,11 +35,11 @@ for i,j in train.iterrows():
     img3 = mpimg.imread('../data/IMG'+x3)
     images.append(img1)
     images.append(img2)
-    images.append(img3)
+    images.append(img3) # adding to list
 for i in train[3]:
-    correction = 0.2
+    correction = 0.2 # assumed correction factor to adjust the bias in steering prediction
     measurements.append(float(i))
-    measurements.append(float(i)+correction) # left
+    measurements.append(float(i)+correction) # left 
     measurements.append(float(i)-correction) # right
 
 
@@ -59,23 +59,21 @@ print(shape)
 
 # In[ ]:
 
-
+# Convolution Neural Network - Nvidia Architecture
 model = Sequential()
 model.add(Lambda(lambda x: x/255.0 -0.5,input_shape=(160,320,3)))
-
-model.add(Convolution2D(32,(5,5),activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.5))
-
-model.add(Convolution2D(64,(5,5),activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Cropping2D(cropping=((70,20),(0,0))))
+model.add(Convolution2D(24,(5,5),strides=(2,2),activation='relu'))
+model.add(Convolution2D(36,(5,5),strides=(2,2),activation='relu'))
+model.add(Convolution2D(48,(5,5),strides=(2,2),activation='relu'))
+model.add(Convolution2D(64,(3,3),activation='relu'))
+model.add(Convolution2D(64,(3,3),activation='relu'))
 model.add(Dropout(0.5))
 
 model.add(Flatten())
-model.add(Dense(120,activation='relu'))
-model.add(Dropout(0.5))
-
-model.add(Dense(84,activation='relu'))
+model.add(Dense(100,activation='relu'))
+model.add(Dense(50,activation='relu'))
+model.add(Dense(10,activation='relu'))
 model.add(Dropout(0.5))
 
 model.add(Dense(1))
@@ -83,8 +81,8 @@ model.add(Dense(1))
 
 # In[ ]:
 
-
+# Training the model
 model.compile(loss='mse',optimizer='Adam')
-model.fit(X_data,y_data,validation_split=0.2,shuffle=True,epochs=2)
+model.fit(X_data,y_data,validation_split=0.2,shuffle=True,epochs=2) # split train data 
 model.save('model.h5')
 
